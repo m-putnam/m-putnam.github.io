3d50452992948bda1107ee2e79b44856ccf82d08
Matthew Lavin <mattlavin@live.com>
2022-07-12 19:39:38 -0400

    Basic WX data fetching

--- wx.js
+++ wx.js
@@ -1,14 +1,40 @@
-window.onload = function()
+window.onload = async function()
 {
-	getStation('KEEN');
+	var stationInfo = await getStation('KEEN');
 	console.log('foo');
+	populateWeather(stationInfo);
 };
 
 async function getStation(name)
 {
-	var url = 'https://api.weather.gov/stations/KEEN/observations/latest';
-	var data = await fetch(url, {
-		mode: 'cors'
+	let url = 'https://api.weather.gov/stations/KEEN/observations/latest';
+	let data = await fetch(url, {
+		mode: 'cors',
 	});
 	console.log('fetched');
+	return await data.json();
+}
+
+function populateWeather(info)
+{
+	let props = info.properties;
+	let box = document.getElementById('weather');
+	let p = document.createElement('p');
+	let observationStamp = new Date(props.timestamp);
+	p.innerText = 'Conditions in Keene, NH circa ' + fmtClockTime(observationStamp);
+	box.appendChild(p);
+}
+
+function fmtClockTime(stamp)
+{
+	let str = stamp.getHours() % 12 + ':';
+	if (!(stamp.getMinutes() % 10))
+		str += '0';
+
+	str += stamp.getMinutes() + ' ';
+	if (stamp.getHours() % 12)
+		str += 'PM';
+	else
+		str += 'AM';
+	return str;
 }
